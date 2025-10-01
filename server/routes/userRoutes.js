323b043a33d90847b7a402a4845f0a168e2b78ef
Anmol Sharma <anmolsharma14003@gmail.com>
2025-10-01 14:59:17 +0530

    change for error 405

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,8 @@
-import express from "express";
-import { getCars, getUserData, loginUser, registerUser } from "../controllers/userController.js";
-import { protect } from "../middleware/auth.js";
-
-const userRouter = express.Router();
-
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
-userRouter.get('/data', protect, getUserData)
-userRouter.get('/cars', getCars)
-
-export default userRouter;
+// Fetch all cars (GET request)
+fetch(`${import.meta.env.VITE_BASE_URL}/api/user/cars`)
+  .then(res => {
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+    return res.json();
+  })
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
